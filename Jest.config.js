b0ed888ebbe13769e6054f781f2e27475c75e5d8
Akash-psg <akashpsgct01@gmail.com>
2024-08-13 14:07:18 +0530

    Initial commit

--- Jest.config.js
+++ Jest.config.js
@@ -1,9 +1,8 @@
-
-  module.exports = {
-    transform: {
-      "^.+.jsx?$": "babel-jest",
-    },
-    moduleFileExtensions: ["js", "jsx","json","node"],
-    extensionsToTreatAsEsm: [".ts", ".tsx",".js"],
-    transformIgnorePatterns: ["/node_modules/(?!(axios)/)"],
-  };
+// module.exports = {
+//     transform: {
+//       "^.+\\.[jt]sx?$": "babel-jest" // Ensure this regex handles both .js and .jsx files
+//     },
+//     moduleFileExtensions: ["js", "jsx", "json", "node"],
+//     transformIgnorePatterns: ["/node_modules/(?!(axios)/)"],
+//     // Remove extensionsToTreatAsEsm if not using ES modules
+//   };
--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
-module.exports = {
-    presets: ["@babel/preset-env", "@babel/preset-react"],
-    plugins: ["@babel/plugin-proposal-private-property-in-object"],
-  };
- 
+// module.exports = {
+//     presets: [
+//       '@babel/preset-env', // For modern JavaScript features
+//       '@babel/preset-react' // For JSX syntax
+//     ],
+//     plugins: [
+//       '@babel/plugin-proposal-private-property-in-object'
+//     ]
+//   };
+  
